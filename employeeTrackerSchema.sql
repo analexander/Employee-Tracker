3d928553c3e2337ca741d5fcf1ac049fa799f2e9
Angel Alexander <angale22@gmail.com>
2020-09-21 17:07:11 -0500

    added employeeTrackerSchema.sql file with table info

---
+++ employeeTrackerSchema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeeTracker_DB;
+CREATE DATABASE employeeTracker_DB;
+
+USE employeeTracker_DB;
+
+CREATE TABLE departments(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL AUTO_INCREMENT,
+  manager_id INT AUTO_INCREMENT,
+  PRIMARY KEY (id)
+);
