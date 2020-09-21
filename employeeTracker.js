aad567244de077e3f92c40d39fb39e9207f8dfde
Angel Alexander <angale22@gmail.com>
2020-09-21 16:44:26 -0500

    added employeeTracker.js file

---
+++ employeeTracker.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "employeeTracker_DB"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
