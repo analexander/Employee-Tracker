cfc5fb7857beb91c27a628c361236f587f700816
Angel Alexander <angale22@gmail.com>
2020-09-21 17:24:07 -0500

    updated sql file with foreign keys

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -14,7 +14,8 @@ CREATE TABLE role(
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -23,5 +24,6 @@ CREATE TABLE employee(
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL AUTO_INCREMENT,
   manager_id INT AUTO_INCREMENT,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY(role_id) REFERENCES role(id)
 );
