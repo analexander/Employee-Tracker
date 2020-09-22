f81fb9723465596d63ac3605a4faf63f0b5c8b80
Angel Alexander <angale22@gmail.com>
2020-09-22 17:18:24 -0500

    fixed .sql file so it can be created in workbench

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -13,17 +13,17 @@ CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL AUTO_INCREMENT,
+  department_id INT NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY(department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL AUTO_INCREMENT,
-  manager_id INT AUTO_INCREMENT,
+  role_id INT NOT NULL,
+  manager_id INT,
   PRIMARY KEY (id),
-  FOREIGN KEY(role_id) REFERENCES role(id)
+  FOREIGN KEY (role_id) REFERENCES role (id)
 );
